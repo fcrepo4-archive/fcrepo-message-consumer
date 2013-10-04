d773264025840011ad758691d76d29c21595ed29
walterMPDL <walter@mpdl.mpg.de>
2013-10-04 10:32:13 +0200

    added SolrIndexer stub
    Integrated a stub version of a SolrIndexer with an embedded Solr server.
    The Solr version is 3.6.2 as modeshape already comes with a lucene-core
    in this version.

--- fcrepo-jms-indexer-core/src/main/java/org/fcrepo/indexer/SolrIndexer.java
+++ fcrepo-jms-indexer-core/src/main/java/org/fcrepo/indexer/SolrIndexer.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2013 DuraSpace, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.fcrepo.indexer;
 
 import java.io.IOException;
@@ -8,51 +24,66 @@ import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.response.UpdateResponse;
 import org.apache.solr.common.SolrInputDocument;
 
+/**
+ * @author walter
+ *
+ */
 public class SolrIndexer implements Indexer {
 
-	
-	private SolrServerFactory solrServerFactory;
-	
-	private SolrServer solrServer;
-	
-	@PostConstruct
-	public void instantiateSolrServer()
-	{
-		this.solrServer = getSolrServerFactory().getSolrServer();
-	}
-	
-	@Override
-	public void update(String pid, String doc) throws IOException {
-		try {
-			SolrInputDocument  inputDoc = new SolrInputDocument();
-			inputDoc.addField("id", pid);
-			inputDoc.addField("content", doc);
-			UpdateResponse resp = solrServer.add(inputDoc);
-			solrServer.commit();
-		} catch (SolrServerException e) {
-			throw new IOException(e);
-		}
-	}
+    private SolrServerFactory solrServerFactory;
 
-	@Override
-	public void remove(String pid) throws IOException {
-		try {
-			solrServer.deleteById(pid);
-		} catch (SolrServerException e) {
-			throw new IOException(e);
-		}
+    private SolrServer solrServer;
 
-	}
+    /**
+     * 
+     */
+    @PostConstruct
+    public void instantiateSolrServer() {
+        this.solrServer = getSolrServerFactory().getSolrServer();
+    }
 
-	public SolrServerFactory getSolrServerFactory() {
-		return solrServerFactory;
-	}
+    /* (non-Javadoc)
+     * @see org.fcrepo.indexer.Indexer#update(java.lang.String, java.lang.String)
+     */
+    @Override
+    public void update(String pid, String doc) throws IOException {
+        try {
+            SolrInputDocument inputDoc = new SolrInputDocument();
+            inputDoc.addField("id", pid);
+            inputDoc.addField("content", doc);
+            UpdateResponse resp = solrServer.add(inputDoc);
+            solrServer.commit();
+        } catch (SolrServerException e) {
+            throw new IOException(e);
+        }
+    }
 
-	public void setSolrServerFactory(SolrServerFactory solrServerFactory) {
-		this.solrServerFactory = solrServerFactory;
-	}
+    /* (non-Javadoc)
+     * @see org.fcrepo.indexer.Indexer#remove(java.lang.String)
+     */
+    @Override
+    public void remove(String pid) throws IOException {
+        try {
+            solrServer.deleteById(pid);
+        } catch (SolrServerException e) {
+            throw new IOException(e);
+        }
 
+    }
 
-	
+    /**
+     * @return SolrServerFactory instance. Needed for generating
+     */
+    public SolrServerFactory getSolrServerFactory() {
+        return solrServerFactory;
+    }
+
+    /**
+     * @param solrServerFactory (SolrServerFactory) a factory for generating
+     * SOLR instances
+     */
+    public void setSolrServerFactory(SolrServerFactory solrServerFactory) {
+        this.solrServerFactory = solrServerFactory;
+    }
 
 }
