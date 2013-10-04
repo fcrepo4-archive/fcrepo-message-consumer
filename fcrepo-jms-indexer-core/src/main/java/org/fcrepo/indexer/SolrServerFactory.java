820f5032dcccda2a347fdd4f5a451791254d0b02
walterMPDL <walter@mpdl.mpg.de>
2013-10-04 10:32:13 +0200

    added SolrIndexer stub
    Integrated a stub version of a SolrIndexer with an embedded Solr server.
    The Solr version is 3.6.2 as modeshape already comes with a lucene-core
    in this version.

--- fcrepo-jms-indexer-core/src/main/java/org/fcrepo/indexer/SolrServerFactory.java
+++ fcrepo-jms-indexer-core/src/main/java/org/fcrepo/indexer/SolrServerFactory.java
@@ -1,62 +1,114 @@
+/**
+ * Copyright 2013 DuraSpace, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.fcrepo.indexer;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.apache.solr.core.CoreContainer;
+import org.apache.solr.core.CoreContainer.Initializer;
+import org.xml.sax.SAXException;
 
+/**
+ * @author walter
+ *
+ */
 public class SolrServerFactory {
 
-	private boolean embedded = false;
-	private String solrServerUrl;
-	private String solrTestHome;
-	private String solrTestConfig;
-	
-	
-	public SolrServer getSolrServer() {
-		if(!isEmbedded())
-		{
-			return new HttpSolrServer(solrServerUrl);
-		
-		}
-		else
-		{
-			//new Corecon
-			CoreContainer cc = new CoreContainer(getSolrTestHome());
-			EmbeddedSolrServer embeddedSolrServer = new EmbeddedSolrServer(cc, "");
-			return embeddedSolrServer;
-		}
-		
-	}
+    private boolean embedded = false;
 
+    private String solrServerUrl;
 
-	public boolean isEmbedded() {
-		return embedded;
-	}
+    private String solrTestHome;
 
+    private String solrTestConfig;
 
-	public void setEmbedded(boolean embedded) {
-		this.embedded = embedded;
-	}
+    /**
+     * @return
+     */
+    public SolrServer getSolrServer() {
+        if (!isEmbedded()) {
+            return new HttpSolrServer(solrServerUrl);
 
+        } else {
+            // new Corecon
+            File file = new File(".");
+            System.out.println(file.getAbsolutePath());
+            System.setProperty("solr.solr.home", solrTestHome);
+            Initializer initializer = new CoreContainer.Initializer();
+            CoreContainer cc = null;
+            try {
+                cc = initializer.initialize();
+            } catch (IOException | ParserConfigurationException | SAXException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            EmbeddedSolrServer embeddedSolrServer =
+                    new EmbeddedSolrServer(cc, "");
+            return embeddedSolrServer;
+        }
 
-	public String getSolrTestHome() {
-		return solrTestHome;
-	}
+    }
 
+    /**
+     * @return
+     */
+    public boolean isEmbedded() {
+        return embedded;
+    }
 
-	public void setSolrTestHome(String solrTestHome) {
-		this.solrTestHome = solrTestHome;
-	}
+    /**
+     * @param embedded
+     */
+    public void setEmbedded(boolean embedded) {
+        this.embedded = embedded;
+    }
 
+    /**
+     * @return
+     */
+    public String getSolrTestHome() {
+        return solrTestHome;
+    }
 
-	public String getSolrTestConfig() {
-		return solrTestConfig;
-	}
+    /**
+     * @param solrTestHome
+     */
+    public void setSolrTestHome(String solrTestHome) {
+        this.solrTestHome = solrTestHome;
+    }
 
+    /**
+     * @return
+     */
+    public String getSolrTestConfig() {
+        return solrTestConfig;
+    }
 
-	public void setSolrTestConfig(String solrTestConfig) {
-		this.solrTestConfig = solrTestConfig;
-	}
-	
+    /**
+     * @param solrTestConfig
+     */
+    public void setSolrTestConfig(String solrTestConfig) {
+        this.solrTestConfig = solrTestConfig;
+    }
+
 }
