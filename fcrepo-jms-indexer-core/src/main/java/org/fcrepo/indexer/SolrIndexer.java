7afa0bd469f3d47b7adebc32e6a25650dbbe73d9
Ye Cao <cao@mpdl.mpg.de>
2013-12-04 16:51:43 +0100

    sync progress

--- fcrepo-jms-indexer-core/src/main/java/org/fcrepo/indexer/SolrIndexer.java
+++ fcrepo-jms-indexer-core/src/main/java/org/fcrepo/indexer/SolrIndexer.java
@@ -16,13 +16,19 @@
 package org.fcrepo.indexer;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 import org.apache.solr.client.solrj.SolrServer;
@@ -46,110 +52,147 @@ import com.hp.hpl.jena.rdf.model.StmtIterator;
  */
 public class SolrIndexer implements Indexer {
 
-    private final SolrServer server;
+	private final SolrServer server;
 
-    private final Logger logger = LoggerFactory.getLogger(SolrIndexer.class);
+	private final List<String> customIndexerFieldsConfig;
 
-    /**
-     * @Autowired solrServer instance is auto-@Autowired in indexer-core.xml
-     */
-    @Autowired
-    public SolrIndexer(final SolrServer solrServer) {
-        this.server = solrServer;
-    }
+	private final Logger logger = LoggerFactory.getLogger(SolrIndexer.class);
 
-    @Override
-    public void update(final String pid, final String doc) {
-        try {
-            final SolrInputDocument inputDoc;
-            final Collection<SolrInputDocument> docs =
-                    new ArrayList<SolrInputDocument>();
-            // inputDoc.addField("id", pid);
-            // inputDoc.addField("content", doc);
-            final HashMap<String, String> tokens =
-                    docParser(pid, doc);
-            final Iterator it = tokens.entrySet().iterator();
-            inputDoc = new SolrInputDocument();
-            while (it.hasNext()) {
-                final Map.Entry pairs = (Map.Entry) it.next();
-                final String fieldname = (String) pairs.getKey();
-                final String fieldvalue = (String) pairs.getValue();
-                // TODO add selected fields
-                if (fieldname
-                        .equals("http://fedora.info/definitions/v4/repository#mixinTypes")) {
-                    inputDoc.addField(fieldname, fieldvalue);
-                    logger.debug("pid:" + pid + "||" + fieldname);
-                }
-                inputDoc.addField("id", pid);
-                docs.add(inputDoc);
-            }
-            final UpdateResponse resp = server.add(docs);
-            if (resp.getStatus() == 0) {
-                // logger.debug("update request was successful for pid: {}",
-                // pid);
-                server.commit();
-            } else {
-                logger.warn(
-                        "update request has error, code: {} for pid: {}",
-                        resp.getStatus(), pid);
-            }
-        } catch (final SolrServerException | IOException e) {
-            logger.warn("Update Exception: {}", e);
-        }
+	/**
+	 * @throws IOException
+	 * @throws URISyntaxException 
+	 * @Autowired solrServer instance is auto-@Autowired in indexer-core.xml
+	 */
+	@Autowired
+	public SolrIndexer(final SolrServer solrServer, String customIndexerFieldsConfigFile) throws IOException, URISyntaxException {
+		this.server = solrServer;
+		this.customIndexerFieldsConfig=customFieldsConfigReader(customIndexerFieldsConfigFile);
+	}
 
-    }
+	@Override
+	public void update(final String pid, final String doc) {
+		try {
+			final SolrInputDocument inputDoc;
+			final Collection<SolrInputDocument> docs =
+					new ArrayList<SolrInputDocument>();
+			final HashMap<String, String> tokens =
+					docParser(pid, doc);
+			final Iterator it = tokens.entrySet().iterator();
+			inputDoc = new SolrInputDocument();
+			final StringBuilder restContent = new StringBuilder("");
+			while (it.hasNext()) {
+				final Map.Entry pairs = (Map.Entry) it.next();
+				final String fieldname = (String) pairs.getKey();
+				final String fieldvalue = (String) pairs.getValue();
+				// add user selected fields
+				for (final String field : this.customIndexerFieldsConfig) {
+					logger.debug("fieldname = " + fieldname);
+					if (fieldname
+							.equals(field)) {
+						inputDoc.addField(fieldname, fieldvalue);
+						logger.debug("pid:" + pid + "||" + fieldname);
+					}
+					else{
+						restContent.append(fieldname+","+fieldvalue+"||");
+					}
+				}
 
-    /**
-     * (non-Javadoc)
-     * @see org.fcrepo.indexer.Indexer#remove(java.lang.String)
-     */
-    @Override
-    public void remove(final String pid) throws IOException {
-        try {
-            final UpdateResponse resp = server.deleteById(pid);
-            if (resp.getStatus() == 0) {
-                logger.debug("remove request was successful for pid: {}", pid);
-                server.commit();
-            } else {
-                logger.warn("remove request has error, code: {} for pid: {}",
-                        resp.getStatus(), pid);
-            }
-        } catch (final SolrServerException | IOException e) {
-            logger.warn("Delete Exception: {}", e);
-        }
+			}
+			if(!restContent.toString().equals("")){
+				inputDoc.addField("content", restContent);
+			}
+			inputDoc.addField("id", pid);
+			docs.add(inputDoc);
+			final UpdateResponse resp = server.add(docs);
+			if (resp.getStatus() == 0) {
+				// logger.debug("update request was successful for pid: {}",
+				// pid);
+				server.commit();
+			} else {
+				logger.warn(
+						"update request has error, code: {} for pid: {}",
+						resp.getStatus(), pid);
+			}
+		} catch (final SolrServerException | IOException e) {
+			logger.warn("Update Exception: {}", e);
+		}
 
-    }
+	}
 
-    /*
-     * return json
-     */
-    HashMap<String, String> docParser(final String inputPid,
-            final String doc) {
-        // parse content into a model
-        final Model model = ModelFactory.createDefaultModel();
-        // final StringReader sr = new StringReader(doc);
-        final InputStream sr = new ByteArrayInputStream(doc.getBytes());
-        model.read(sr, "N3");
-        final StmtIterator iter = model.listStatements();
-        final HashMap<String, String> fields = new HashMap<String, String>();
-        while (iter.hasNext()) {
-            final String str0 = iter.next().toString();
-            // get rid of [... ]
-            final String str = str0.substring(1, str0.length() - 1);
-            final String delims = ",";
-            final StringTokenizer st = new StringTokenizer(str, delims);
-            if (st.countTokens() == 3) {
-                final String pid = st.nextToken();
-             // check if contains pid
-                if (inputPid.equals(pid)) {
-                    // insert key
-                    final String fieldName = st.nextToken().trim();
-                    // insert value
-                    final String fieldValue = st.nextToken().trim();
-                    fields.put(fieldName, fieldValue);
-                }
-            }
-        }
-        return fields;
-    }
+	/**
+	 * (non-Javadoc)
+	 * @see org.fcrepo.indexer.Indexer#remove(java.lang.String)
+	 */
+	@Override
+	public void remove(final String pid) throws IOException {
+		try {
+			final UpdateResponse resp = server.deleteById(pid);
+			if (resp.getStatus() == 0) {
+				logger.debug("remove request was successful for pid: {}", pid);
+				server.commit();
+			} else {
+				logger.warn("remove request has error, code: {} for pid: {}",
+						resp.getStatus(), pid);
+			}
+		} catch (final SolrServerException | IOException e) {
+			logger.warn("Delete Exception: {}", e);
+		}
+
+	}
+
+	/*
+	 * return json
+	 */
+	HashMap<String, String> docParser(final String inputPid,
+			final String doc) {
+		// parse content into a model
+		final Model model = ModelFactory.createDefaultModel();
+		// final StringReader sr = new StringReader(doc);
+		final InputStream sr = new ByteArrayInputStream(doc.getBytes());
+		model.read(sr, null,"N3");
+		final StmtIterator iter = model.listStatements();
+		final HashMap<String, String> fields = new HashMap<String, String>();
+		while (iter.hasNext()) {
+			final String str0 = iter.next().toString();
+			// get rid of [... ]
+			final String str = str0.substring(1, str0.length() - 1);
+			final String delims = ",";
+			final StringTokenizer st = new StringTokenizer(str, delims);
+			if (st.countTokens() == 3) {
+				final String pid = st.nextToken();
+				// check if contains pid
+				if (inputPid.equals(pid)) {
+					// insert key
+					final String fieldName = st.nextToken().trim();
+					// insert value
+					final String fieldValue = st.nextToken().trim();
+					fields.put(fieldName, fieldValue);
+				}
+			}
+		}
+		return fields;
+	}
+
+	List<String> customFieldsConfigReader(String filepath)throws IOException, URISyntaxException{
+		final List<String> result = new ArrayList<String>();
+		final String FIELD_NAME_PATTERN = "name=";
+		URL fileUrl = SolrIndexer.class.getResource(filepath);
+		String config = new String(java.nio.file.Files.readAllBytes(Paths.get(fileUrl.toURI())), "UTF8");
+		String[] lines = config.split(System.getProperty("line.separator"));
+		for (int i = 0; i < lines.length; i++){
+			final String thisline = lines[i];
+			final int fieldnameFound = thisline.indexOf(FIELD_NAME_PATTERN);
+			if (fieldnameFound != -1) {
+				final int start = fieldnameFound + FIELD_NAME_PATTERN.length()
+						+ 1;
+				final int end = thisline.indexOf('"', start);
+				result.add(thisline.substring(start, end));
+			}
+		}
+		
+		
+		
+		return result;
+	}
+
 }
