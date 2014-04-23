2aae652747f748f0d0bc81eb360334e4e66963d9
nikhil trivedi <ntrivedi@artic.edu>
2014-04-23 15:04:55 -0500

    Initial work on tests.

--- fcrepo-jms-indexer-core/src/main/java/org/fcrepo/indexer/IndexerGroup.java
+++ fcrepo-jms-indexer-core/src/main/java/org/fcrepo/indexer/IndexerGroup.java
@@ -25,7 +25,6 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.PoolingClientConnectionManager;
